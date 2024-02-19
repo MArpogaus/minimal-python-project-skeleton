27ac4ed5ce34b1e6f4a5e5820becdec55af06a6a
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-02-19 15:30:39 +0100

    refactor!: rename func -> increment

--- test/test_func.py
+++ test/test_func.py
@@ -1,5 +1,9 @@
-from minimal_python_project_skeleton import func
+from minimal_python_project_skeleton import increment
 
 
-def test_answer():
-    assert func(3) == 4
+def test_int():
+    assert increment(3) == 4
+
+
+def test_float():
+    assert increment(3.0) == 4.0
